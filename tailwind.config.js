1e9bc999ce932cf06563230482a282bfbf5a36af
m-schox <mehul@schox.com>
2025-05-30 15:17:03 -0700

    update styling and use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx,html}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif'],
+        'cousine': ['Cousine', 'monospace']
+      },
+      colors: {
+        'custom-dark': '#18272F',
+        'custom-light': '#F5F5F5',
+        'custom-teal': '#2e898e8a'
+      }
+    }
+  },
+  plugins: []
+} 
